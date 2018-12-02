8d0e66c2b8fa3da40062b0d62d11e5e90bd14158
Andy Miller <andy@andyfmiller.com>
2018-12-02 14:04:19 -0800

    Change service_version to service_versions

--- src/LtiAdvantage/NamesRoleProvisioningService/NamesRoleServiceClaimValueType.cs
+++ src/LtiAdvantage/NamesRoleProvisioningService/NamesRoleServiceClaimValueType.cs
@@ -14,15 +14,6 @@ namespace LtiAdvantage.NamesRoleProvisioningService
         /// </summary>
         public const string Version = "2.0";
 
-        ///// <summary>
-        ///// Create an instance of the <see cref="NamesRoleServiceClaimValueType"/> with
-        ///// the default value for <see cref="ServiceVersion"/>.
-        ///// </summary>
-        //public NamesRoleServiceClaimValueType()
-        //{
-        //    ServiceVersion = Version;
-        //}
-
         /// <summary>
         /// Fully resolved URL to service.
         /// </summary>
@@ -32,8 +23,8 @@ namespace LtiAdvantage.NamesRoleProvisioningService
         /// <summary>
         /// Service version. Default is <see cref="Version"/>.
         /// </summary>
-        [JsonProperty("service_version")]
-        [DefaultValue(Version)]
-        public string ServiceVersion { get; set; }
+        [JsonProperty("service_versions")]
+        [DefaultValue(new [] {Version})]
+        public string[] ServiceVersions { get; set; }
     }
 }

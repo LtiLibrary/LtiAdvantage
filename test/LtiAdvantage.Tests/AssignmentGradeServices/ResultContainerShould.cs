3fe73e4902a181eb31b662d539c561e55d51f270
Andy Miller <andy@andyfmiller.com>
2019-01-27 11:59:11 -0800

    Add ResultContainer tests

---
+++ test/LtiAdvantage.Tests/AssignmentGradeServices/ResultContainerShould.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LtiAdvantage.AssignmentGradeServices;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace LtiAdvantage.Tests.AssignmentGradeServices
+{
+    public class ResultContainerShould
+    {
+        /// <summary>
+        /// Deserialize the example result container from IMS documentation and check values.
+        /// See https://www.imsglobal.org/spec/lti-ags/v2p0/#example-of-an-initial-application-vnd-ims-lis-v2-resultcontainer-json-
+        /// </summary>
+        [Fact]
+        public void DeserializeFromValueJson()
+        {
+            var referenceJson = TestUtils.LoadReferenceJsonFile("ResultContainer");
+            var container = JsonConvert.DeserializeObject<ResultContainer>(referenceJson);
+
+            Assert.Equal(1, container.Count);
+        }
+        
+        /// <summary>
+        /// Deserialize the example result container from IMS documentation and then serialize back to JSON
+        /// and compare JSON.
+        /// </summary>
+        [Fact]
+        public void SerializeToValidJson()
+        {
+            var referenceJson = TestUtils.LoadReferenceJsonFile("ResultContainer");
+            var container = JsonConvert.DeserializeObject<ResultContainer>(referenceJson);
+            var containerJson = JsonConvert.SerializeObject(container);
+
+            JsonAssertions.Equal(referenceJson, containerJson);
+        }
+    }
+}

8aed1650ed6bb646d0f30951720531ed42c4d84f
Andy Miller <andy@andyfmiller.com>
2019-01-27 11:46:58 -0800

    Add LineItemContainer tests

---
+++ test/LtiAdvantage.Tests/AssignmentGradeServices/LineItemContainerShould.cs
@@ -0,0 +1,36 @@
+﻿using LtiAdvantage.AssignmentGradeServices;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace LtiAdvantage.Tests.AssignmentGradeServices
+{
+    public class LineItemContainerShould
+    {
+        /// <summary>
+        /// Deserialize the example lineitem container from IMS documentation and check values.
+        /// See https://www.imsglobal.org/spec/lti-ags/v2p0/#example-application-vnd-ims-lis-v2-lineitemcontainer-json-representation
+        /// </summary>
+        [Fact]
+        public void DeserializeFromValueJson()
+        {
+            var referenceJson = TestUtils.LoadReferenceJsonFile("LineItemContainer");
+            var container = JsonConvert.DeserializeObject<LineItemContainer>(referenceJson);
+
+            Assert.Equal(3, container.Count);
+        }
+        
+        /// <summary>
+        /// Deserialize the example lineitem container from IMS documentation and then serialize back to JSON
+        /// and compare JSON.
+        /// </summary>
+        [Fact]
+        public void SerializeToValidJson()
+        {
+            var referenceJson = TestUtils.LoadReferenceJsonFile("LineItemContainer");
+            var container = JsonConvert.DeserializeObject<LineItemContainer>(referenceJson);
+            var containerJson = JsonConvert.SerializeObject(container);
+
+            JsonAssertions.Equal(referenceJson, containerJson);
+        }
+    }
+}

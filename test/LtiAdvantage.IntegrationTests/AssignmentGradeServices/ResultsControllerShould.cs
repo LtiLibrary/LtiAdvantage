44a631130f314c47185602c299edcf40e1e2aecc
Andy Miller <andy@andyfmiller.com>
2019-01-29 10:46:49 -0800

    Add ResultsController integration tests

---
+++ test/LtiAdvantage.IntegrationTests/AssignmentGradeServices/ResultsControllerShould.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using LtiAdvantage.AssignmentGradeServices;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace LtiAdvantage.IntegrationTests.AssignmentGradeServices
+{
+    public class ResultsControllerShould : IDisposable
+    {
+        private readonly HttpClient _client;
+        private readonly TestServer _server;
+
+        private const string ResultsUrl = "context/1234/lineitems/1234/results";
+
+        public ResultsControllerShould()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        /// <summary>
+        /// Check the return status code and content-type.
+        /// </summary>
+        [Theory]
+        [InlineData(Constants.LtiScopes.AgsResultReadonly, HttpStatusCode.OK, Constants.MediaTypes.ResultContainer)]
+        [InlineData(Constants.LtiScopes.AgsLineItemReadonly, HttpStatusCode.Forbidden, "")]
+        public async void ReturnResult_WhenScopeAllows(string scope, HttpStatusCode statusCode, string contentType)
+        {
+            _client.DefaultRequestHeaders.Add("x-test-scope", scope);
+            var response = await _client.GetAsync(ResultsUrl);
+            Assert.Equal(statusCode, response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                Assert.StartsWith(contentType, response.Content.Headers.ContentType.ToString());
+            }
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+            _server?.Dispose();
+        }
+    }
+}
---
+++ test/LtiAdvantage.IntegrationTests/Controllers/ResultsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using LtiAdvantage.AssignmentGradeServices;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LtiAdvantage.IntegrationTests.Controllers
+{
+    public class ResultsController : ResultsControllerBase
+    {
+        public ResultsController(IHostingEnvironment env, ILogger<ResultsControllerBase> logger) : base(env, logger)
+        {
+        }
+
+        protected override Task<ActionResult<ResultContainer>> OnGetResultsAsync(GetResultsRequest request)
+        {
+            var result = (ActionResult<ResultContainer>) new ObjectResult(new ResultContainer());
+            return Task.FromResult(result);
+        }
+    }
+}

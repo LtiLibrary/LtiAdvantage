0018544baf56e58da51518312b05f08e7a9d7a43
Andy Miller <andy@andyfmiller.com>
2019-01-29 12:04:11 -0800

    Add MembershipController integration tests

---
+++ test/LtiAdvantage.IntegrationTests/Controllers/MembershipController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using LtiAdvantage.NamesRoleProvisioningService;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LtiAdvantage.IntegrationTests.Controllers
+{
+    public class MembershipController : MembershipControllerBase
+    {
+        public MembershipController(IHostingEnvironment env, ILogger<MembershipControllerBase> logger) : base(env, logger)
+        {
+        }
+
+        protected override Task<ActionResult<MembershipContainer>> OnGetMembershipAsync(GetMembershipRequest request)
+        {
+            var result = (ActionResult<MembershipContainer>) new ObjectResult(new MembershipContainer());
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ test/LtiAdvantage.IntegrationTests/NamesRoleProvisioningService/MembershipControllerShould.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace LtiAdvantage.IntegrationTests.NamesRoleProvisioningService
+{
+    public class MembershipControllerShould : IDisposable
+    {
+        private readonly HttpClient _client;
+        private readonly TestServer _server;
+
+        private const string MembershipUrl = "context/1234/membership";
+
+        public MembershipControllerShould()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        /// <summary>
+        /// Check the return status code and content-type.
+        /// </summary>
+        [Theory]
+        [InlineData(Constants.LtiScopes.NrpsMembershipReadonly, HttpStatusCode.OK, Constants.MediaTypes.MembershipContainer)]
+        [InlineData(Constants.LtiScopes.AgsLineItem, HttpStatusCode.Forbidden, "")]
+        public async void ReturnMembership_WhenScopeAllows(string scope, HttpStatusCode statusCode, string contentType)
+        {
+            _client.DefaultRequestHeaders.Add("x-test-scope", scope);
+            var response = await _client.GetAsync(MembershipUrl);
+            Assert.Equal(statusCode, response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                Assert.StartsWith(contentType, response.Content.Headers.ContentType.ToString());
+            }
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+            _server?.Dispose();
+        }
+    }
+}
